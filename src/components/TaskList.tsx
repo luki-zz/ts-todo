8efc51c53d5381ade77431ba2d514bdd1f532809
luki-zz <luki.wojnar@gmail.com>
2023-01-12 20:24:10 +0100

    move css partialy to tasklis module

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { TaskModel } from "./task.model";
+import styles from "./taskList.module.css";
 
 export type TasksProps = {
   tasks: TaskModel[];
@@ -8,9 +9,9 @@ export type TasksProps = {
 
 export const TaskList = (props: TasksProps): JSX.Element => {
   return (
-    <ul>
+    <ul className={styles.taskList}>
       {props.tasks.map((task: TaskModel) => (
-        <li key={task.id}>
+        <li className={styles.taskListItem} key={task.id}>
           {task.taskName}
           <button onClick={() => props.onRemoveTask(task.id)}>Remove</button>
         </li>
